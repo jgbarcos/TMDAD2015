dbb2c764fb7e389aa7db8017ca066b8f5c55e1f6
Alex <610841@unizar.es>
2016-04-10 21:31:54 +0200

    Tokenizer function in process

---
+++ prototypes/webgui/src/main/java/es/unizar/tmdad/lab0/service/Token.java
@@ -0,0 +1,25 @@
+package es.unizar.tmdad.lab0.service;
+
+public class Token {
+	private String word;
+	private int count;
+	
+	public Token(String word, int count) {
+		this.word = word;
+		this.count = count;
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+}
