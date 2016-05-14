ee8856b209de4e4b06f83831c14ac5e260a367b9
erremesse <551940@unizar.es>
2016-05-14 18:58:47 +0200

    Tokenizer divided in two components

--- prototypes/tokenizer/src/main/java/es/unizar/tmdad/controller/TokenizerController.java
+++ prototypes/tokenizer/src/main/java/es/unizar/tmdad/controller/TokenizerController.java
@@ -2,7 +2,7 @@ package es.unizar.tmdad.controller;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import es.unizar.tmdad.service.BookRaw;
 import es.unizar.tmdad.service.BookTokenized;
-
+import es.unizar.tmdad.tokenizer.Counter;
 import es.unizar.tmdad.tokenizer.Tokenizer;
 import es.unizar.tmdad.tokenizer.TokenizerDummy;
 
@@ -23,30 +23,21 @@ public class TokenizerController {
 	@RequestMapping(method=RequestMethod.POST)
 	public BookTokenized tokenize(@RequestBody BookRaw bookRaw, @RequestParam("terms[]") List<String> terms){ 
 		
-		//TODO BookRaw -> BookTokenized
+		//BookRaw -> BookTokenized
 		System.out.println("BOOK CONTENT: " + bookRaw.getContent().substring(0, 200) + "...");
 		System.out.println(terms);
 		
 		BookTokenized bookTok = new BookTokenized(bookRaw.getId());
 		try{
-			Tokenizer tokenizer = new Tokenizer(bookRaw.getContent(), terms, bookTok);
-			return tokenizer.tokenize();
+			Tokenizer tokenizer = new Tokenizer(bookRaw.getContent());
+			Map<String, String> chapters = tokenizer.tokenize();
+			Counter counter = new Counter(bookTok, chapters, terms);
+			return counter.countTerms();
 		}catch(IOException ioex){
-			//Do nothing...
+			//TODO It should return some standard error
 			System.out.println(ioex);
 		}
 		return null;
 	}
 	
-//	private void createChapter(BookTokenized bookTok, List<String> terms, int id, String chTitle){
-//		Chapter ch = new Chapter(id, chTitle);
-//		// Fill with random terms
-//		Random rnd = new Random();
-//		for(String t : terms){
-//			if(rnd.nextFloat() < 0.6){
-//				ch.addToken(t, rnd.nextInt(10)+1);
-//			}
-//		}		
-//		bookTok.getChapters().add(ch);
-//	}
 }
--- prototypes/tokenizer/src/main/java/es/unizar/tmdad/tokenizer/Tokenizer.java
+++ prototypes/tokenizer/src/main/java/es/unizar/tmdad/tokenizer/Tokenizer.java
@@ -19,8 +19,6 @@ import es.unizar.tmdad.service.Token;
 
 public class Tokenizer {
 	private String bookContent;
-	private List<String> terms;
-	private BookTokenized bookTok;
 	
 	private Map<String, String> chapterMap;
 	
@@ -29,22 +27,17 @@ public class Tokenizer {
 	private static final String titleDelimiter = "Title";	
 	private static final Pattern titlePattern = Pattern.compile(titleDelimiter);
 	
-	public Tokenizer(String bookContent, List<String> terms, BookTokenized bookTok) {
+	public Tokenizer(String bookContent) {
 		super();
 		this.bookContent = bookContent;
-		this.terms = terms;
-		this.bookTok = bookTok;
 		this.chapterMap = new HashMap<String, String>();
 	}
 	
-	public BookTokenized tokenize() throws IOException{
+	public Map<String, String> tokenize() throws IOException{
 		//I've call this function tokenize but firtstly it fill create a structure of chapters
 		//and then it will count tokens on each chapter.
 		BufferedReader br = new BufferedReader(new StringReader(bookContent));
-		//Just to test
-//		FileInputStream fstream = new FileInputStream("src\\main\\resources\\Alice's Adventures in Wonderland"); 
-//		BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-		//
+
 		String line;
 		StringBuilder sb = null;
 		String chapterTitle = "";
@@ -52,7 +45,7 @@ public class Tokenizer {
 	    while ((line = br.readLine()) != null) {
 	    	if(titlePattern.matcher(line).find()){
 //	    	if(line.contains(titleDelimiter)){
-	    		bookTok.setTitle(line.split(":")[1].substring(1)); 
+	    		chapterMap.put("Title", line.split(":")[1].substring(1));
 //	    	} else if (line.contains(chapterDelimiter)){
 	    	} else if (chapterPattern.matcher(line).find()) {
 	    		if(sb != null){
@@ -70,28 +63,7 @@ public class Tokenizer {
 	    	}
 	    } //TODO Migrate to Java 8 streams interface
 	    br.close();
-	    
-	    //Just to test
-	    int i = 0;
-	    for (Map.Entry<String, String> entry : chapterMap.entrySet()) {
-	        String cTitle = entry.getKey();
-	        String cContent = entry.getValue();
-	        Chapter chapter = new Chapter(i, cTitle);
-	        bookTok.addChapter(chapter);
-	        countChapterTokens(chapter, cContent, terms);
-	        i++;
-	    } //TODO Migrate to Java 8 streams interface
-
-		return bookTok;
-	}
-
-	private void countChapterTokens(Chapter chapter, String cContent, List<String> terms) {
-		for (String term: terms){
-			int occurance = StringUtils.countMatches(cContent, term);
-			if(occurance != 0){
-				chapter.addToken(new Token(term, occurance));
-			}
-		}
+	    return chapterMap;
 	}
 	
 }
