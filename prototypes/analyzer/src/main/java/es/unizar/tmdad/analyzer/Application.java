ad63d1489ffbe660d5482e3d1fc9b0df350bfd57
erremesse <551940@unizar.es>
2016-06-06 01:34:12 +0200

    Analyzer main class updated to be deployed on Tomcat

--- prototypes/analyzer/src/main/java/es/unizar/tmdad/analyzer/Application.java
+++ prototypes/analyzer/src/main/java/es/unizar/tmdad/analyzer/Application.java
@@ -1,14 +1,31 @@
 package es.unizar.tmdad.analyzer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@SpringBootApplication
-public class Application {
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+    	//Launches Spring -> Rest Service
+    	SpringApplication.run(Application.class, args);
     }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+    
+    private static Class<Application> applicationClass = Application.class;
+
 }
