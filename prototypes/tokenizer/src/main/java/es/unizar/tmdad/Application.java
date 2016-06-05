b827b35e16b6afbfe8cb31bdf9f8e5653c54d45b
erremesse <551940@unizar.es>
2016-06-06 00:50:18 +0200

    Tokenizer main class updated to be deployed on Tomcat

--- prototypes/tokenizer/src/main/java/es/unizar/tmdad/Application.java
+++ prototypes/tokenizer/src/main/java/es/unizar/tmdad/Application.java
@@ -1,12 +1,19 @@
 package es.unizar.tmdad;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import es.unizar.tmdad.messaging.TokenizerRPCServer;
 
-@SpringBootApplication
-public class Application {
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
     	//Launches Spring -> Rest Service
@@ -15,4 +22,12 @@ public class Application {
         //Launches Rabbit Client -> Messaging Service
         TokenizerRPCServer.launch();
     }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+    
+    private static Class<Application> applicationClass = Application.class;
+    
 }
