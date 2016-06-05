ab914a6b7f1c117c0fb81f5b4b4d9259c14f3441
erremesse <551940@unizar.es>
2016-06-06 01:13:32 +0200

    Gateway main class updated to be deployed on Tomcat

--- prototypes/gateway/src/main/java/es/unizar/tmdad/lab0/Application.java
+++ prototypes/gateway/src/main/java/es/unizar/tmdad/lab0/Application.java
@@ -1,12 +1,19 @@
 package es.unizar.tmdad.lab0;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import es.unizar.tmdad.lab0.messaging.GatewayRPCServer;
 
-@SpringBootApplication
-public class Application {
+//@SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
     	//Launches Spring -> Rest Service
@@ -15,4 +22,11 @@ public class Application {
         //Launches Rabbit Client -> Messaging Service
         GatewayRPCServer.launch();
     }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
+    
+    private static Class<Application> applicationClass = Application.class;
 }
