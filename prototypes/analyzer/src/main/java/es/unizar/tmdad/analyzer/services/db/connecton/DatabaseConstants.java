c72e65a2c136e3710bdfc7445eaeda739ab5b157
victoria <victoria@victoria-SATELLITE-P50-A-14G>
2016-06-10 01:50:08 +0200

    Database credentials changed and MySQL Driver

--- prototypes/analyzer/src/main/java/es/unizar/tmdad/analyzer/services/db/connecton/DatabaseConstants.java
+++ prototypes/analyzer/src/main/java/es/unizar/tmdad/analyzer/services/db/connecton/DatabaseConstants.java
@@ -1,8 +1,8 @@
 package es.unizar.tmdad.analyzer.services.db.connecton;
 
 public class DatabaseConstants {
-	public static final String dbUser = "root";
-	public static final String dbPass = "root";
+	public static final String dbUser = "TMDAD16";
+	public static final String dbPass = "TMDAD16";
 	public static final String dbUrl = "jdbc:mysql://localhost:3306";
 	public static final String dbSchema = "TMDAD16";
 }
